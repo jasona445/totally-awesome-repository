2d67a501046573beebb2bceec5ef94817f4bdc1e
mcchu1217 <michael.chu@yale.edu>
2022-07-03 16:08:52 -0400

    psuedo-code

--- phenotype-eQTL.R
+++ phenotype-eQTL.R
@@ -4,8 +4,28 @@ pheno_eQTL_comparison <- function(eQTLs, phenotypes)
 {
   #get the phenotype data 
   #for each phenotype, get the associated loci for the phenotype
-  getLoci(summary_phenotype)
-  #run jlim on the eqtls and phenotypes that share loci 
-  apply()
-  
+  for(i in 1:nrows(phenotypes))
+  {
+    #here we are getting the associated loci for a given phenotype
+    #using the function we created for the first part
+    phenotype_loci <- getLoci(phenotypes[i,])
+    #one of the objects that the function returns is the list of lead SNPs
+    #we want to run JLIM on pairs of phenotypes and eQTLs that are associated with the same locus 
+    #so for each eQTL, if the SNP is within a 1mBP window of a significant SNP for the phenotype
+    #we will add it to the file of eQTLs to compare with the phenotype/trait.
+    eQTLs_to_test <- c()
+    for(j in 1:nrows(eQTLs))
+    {
+      #this should return a vector or list of the absolute differences between the eQTL's SNP BP and the BPs of phenotypes significant SNPs
+      diffs <- abs(eQTLs$BP[j,] - phenotype_loci[1])
+      #if any of them are within the window then add the eQTL to the list 
+      if(min(diffs) < 1e6)
+      {
+        eQTLs_to_test = c(eQTLs_to_test, eQTLs[j,])
+      }
+      #run JLIM on the eQTLs and the phenotype 
+      #probably to some output file
+      JLIM(eQTLs_to_test, phenotype[i,])
+    }
+  }
 }
