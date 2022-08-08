97d67f15f0455c7d0ce0667c17137a47ccd5f590
mcchu1217 <michael.chu@yale.edu>
2022-08-08 15:39:56 -0400

    pheno gwas

--- runDaGWAS.sh
+++ runDaGWAS.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/bash
-
-#make loop over all phenotypes, probably just need to make a list of all their names and loop over that
-name="MFI_CD16_in_CD16hi_of_NKnew.panel4"
-./plink --bfile ./Data/pasteur/homes/epatin/LabExMI_imputation_816x5699237 --linear sex --pheno ./Data/phenotypes_normalized.txt --pheno-name ${name} --out ./Data/GWAS_Results/${name}
+./plink --bfile ./Data/pasteur/homes/epatin/LabExMI_imputation_816x5699237 --linear sex hide-covar --pheno ./Data/phenotypes_linear.txt --all-pheno --covar ./Data/selected_covars.txt --out ./Data/gwas_results/MIP_linear
+./plink --bfile ./Data/pasteur/homes/epatin/LabExMI_imputation_816x5699237 --logistic sex hide-covar --pheno ./Data/phenotypes_logistic.txt --all-pheno --covar ./Data/selected_covars.txt --out ./Data/gwas_results/MIP_logistic
